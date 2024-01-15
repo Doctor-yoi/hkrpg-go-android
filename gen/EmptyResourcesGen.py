0e31879d8ce66779987a4a2ce8c74a2e63297f6b
Doctor-yoi <106463935+Doctor-yoi@users.noreply.github.com>
2024-01-15 14:28:58 +0800

    更新 EmptyResourcesGen.py

--- gen/EmptyResourcesGen.py
+++ gen/EmptyResourcesGen.py
@@ -1,1 +1,102 @@
-a
+import os
+import base64
+import time
+
+ResourcesFileNameList_ExcelOutput = [
+    "AvatarConfig.json",
+    "ActivityLoginConfig.json",
+    "ActivityPanel.json",
+    "AvatarDemoConfig.json",
+    "AvatarExpItemConfig.json",
+    "AvatarPromotionConfig.json",
+    "AvatarSkillTreeConfig.json",
+    "BackGroundMusic.json",
+    "ChallengeMazeConfig.json",
+    "ChallengeStoryMazeExtra.json",
+    "ChallengeTargetConfig.json",
+    "CocoonConfig.json",
+    "EquipmentConfig.json",
+    "EquipmentExpItemConfig.json",
+    "EquipmentExpType.json",
+    "EquipmentPromotionConfig.json",
+    "ExpType.json",
+    "ItemConfig.json",
+    "ItemConfigAvatar.json",
+    "ItemConfigAvatarPlayerIcon.json",
+    "ItemConfigAvatarRank.json",
+    "ItemConfigBook.json",
+    "ItemConfigDisk.json",
+    "ItemConfigEquipment.json",
+    "ItemConfigRelic.json",
+    "ItemConfigEquipment.json",
+    "LoadingDesc.json",
+    "MapEntrance.json",
+    "MappingInfo.json",
+    "MazeBuff.json",
+    "MazePlane.json",
+    "MazeProp.json",
+    "MonsterConfig.json",
+    "NPCData.json",
+    "NPCMonsterData.json",
+    "PlaneEvent.json",
+    "PlayerLevelConfig.json",
+    "QuestData.json",
+    "RelicConfig.json",
+    "RelicExpItem.json",
+    "RelicExpType.json",
+    "RelicMainAffixConfig.json",
+    "RelicSubAffixConfig.json",
+    "RewardData.json",
+    "RogueAreaConfig.json",
+    "RogueMap.json",
+    "RogueRoom.json",
+    "RogueTalent.json",
+    "ShopConfig.json",
+    "ShopGoodsConfig.json",
+    "SpecialAvatar.json",
+    "StageConfig.json",
+    "TextJoinConfig.json"
+]
+
+if __name__ == "__main__":
+    print("hkrpg-go resources2go")
+    print("author: github@Doctor-yoi")
+    print("outFile: $ProjectRoot$/gameData/Resources_b64.go")
+
+    goCode = "///    Generate By EmptyResourcesGen.py"
+    goCode += "\n///    DO NOT EDIT!!!"
+    goCode += "\n///    Generate Time: " + time.asctime(time.localtime())
+    goCode += "\npackage gameData"
+
+    print("Translate ExcelOutput...")
+    goCode += "\n\nvar ("
+    for file in ResourcesFileNameList_ExcelOutput:
+        (fileName, fileSuffix) = os.path.splitext(file)
+        goLine = "\n    " + fileName + " = \"" + "\""
+        goCode += goLine
+    goCode += "\n)"
+
+    print("Translate Floor...")
+    goCode += "\nvar floorMap := nil
+    goCode += "\nfunc FloorMap() map[string]string {"
+    goCode += "\n    if !(floorMap == nil) {
+    goCode += "\n        floorMap = make(map[string]string,0)"
+    goCode += "\n    }"
+    goCode += "\n    return floorMap"
+    goCode += "\n}"
+
+    print("Translate Group...")
+    goCode += "\nvar groupMap := nil
+    goCode += "\nfunc GroupMap() map[string]string {"
+    goCode += "\n    if !(groupMap == nil) {
+    goCode += "\n        groupMap = make(map[string]string,0)"
+    goCode += "\n    }"
+    goCode += "\n    return groupMap
+    goCode += "\n}"
+
+    print("Saving...")
+    with open(os.path.abspath("..") + "\\gameData\\Resources_b64.go", "w") as goFile:
+        goFile.write(goCode)
+
+    print('Done.')
+    exit(0)
